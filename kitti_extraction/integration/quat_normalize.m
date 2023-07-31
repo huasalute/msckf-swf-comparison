14c86f4a2ec6ce1cdca798de29a63eeef87de193
hua.salute <138840699+huasalute@users.noreply.github.com>
2023-07-31 09:36:43 +0800

    Update quat_normalize.m

--- kitti_extraction/integration/quat_normalize.m
+++ kitti_extraction/integration/quat_normalize.m
@@ -1,4 +1,4 @@
-function [qn] = quat_normalize(q)
+function [qn] = quat_normalize(a)
 % QUAT_NORMALIZE Normalize quaternion.
 %
 %   [qn] = QUAT_NORMALIZE(q) returns the normalized verion of q, with unit
